3c2abb3eaf24098a0294c2f7376b35ede59c4f96
Milovan Gal <email@milovangal.de>
2015-11-06 23:38:39 +0100

    Fix last wrong file commit

--- app/code/local/Welance/Kraken/Block/Product/Image.php
+++ app/code/local/Welance/Kraken/Block/Product/Image.php
@@ -1,25 +1,70 @@
 <?php
 
-if(Mage::helper('core')->isModuleEnabled('Netzarbeiter_NicerImageNames')) {
-  class KrakenFakeImageModel extends Netzarbeiter_NicerImageNames_Model_Image {}
-} else {
-  class KrakenFakeImageModel extends Mage_Catalog_Model_Product_Image {}
-}
-
-class Welance_Kraken_Model_Product_Image extends KrakenFakeImageModel
+class Welance_Kraken_Block_Product_Image extends Mage_Catalog_Block_Product_View
 {
-
-    /**
-     * @return Mage_Catalog_Model_Product_Image
-     */
-    public function saveFile()
-    {
-        parent::saveFile();
-
-        /* added for Kraken Image Optimization */
-        Mage::dispatchEvent('catalog_product_image_save_after', array($this->_eventObject => $this));
-
-        return $this;
+    public function getConcurrency(){
+        return Mage::getStoreConfig('welance_kraken/kraken_config/frontend_concurrency');
     }
 
+    public function getImagesToOptimize()
+    {
+        $product = $this->getProduct();
+
+        $productImages = $this->_getProductImages($product);
+
+        $resource = Mage::getSingleton('core/resource');
+
+        $readConnection = $resource->getConnection('core_read');
+
+        $table = $resource->getTableName('welance_kraken/image_cache');
+
+        $query = "SELECT * FROM {$table} WHERE (`is_running` = 0 AND `is_processed` = 0) AND (`file_name` LIKE ";
+        $i = 0;
+        $productImagesCount = count($productImages);
+
+        foreach ($productImages as $productImage) {
+          if(Mage::helper('core')->isModuleEnabled('Netzarbeiter_NicerImageNames')) {
+            $imageExt = '.' . end(explode('.', $productImage));
+            $query .= "'%". str_replace($imageExt, '/', $productImage) ."%'";
+          } else {
+            $query .= "'%". $productImage ."%'";
+          }
+
+
+
+            $i++;
+
+            if ($i < $productImagesCount) {
+                $query .= " OR `file_name` LIKE ";
+            } else {
+                $query .= ")";
+            }
+        }
+
+        $cacheImages = array();
+
+        if ($productImagesCount > 0) {
+            foreach($readConnection->fetchAll($query) as $cacheImage){
+                $cacheImages[] = array(
+                    'id' =>$cacheImage['id'],
+                    'product_id' => $product->getId()
+                );
+            }
+        }
+
+        return json_encode($cacheImages);
+    }
+
+    protected function _getProductImages($product)
+    {
+        $images = $product->getMediaGalleryImages();
+
+        $productImages = array();
+
+        foreach($images as $image){
+            $productImages[] = $image['file'];
+        }
+
+        return $productImages;
+    }
 }
